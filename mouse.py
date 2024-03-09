81651f7638dcc1e9000d8e1ba9dbf9a743f3321a
AYUSH KUMAR <125579641+ayushmarch94@users.noreply.github.com>
2024-03-09 16:49:33 +0530

    Added code describtion using comment

--- mouse.py
+++ mouse.py
@@ -1,18 +1,21 @@
-import cv2
-import mediapipe as mp
-import pyautogui
-cap = cv2.VideoCapture(0)
-hand_detector = mp.solutions.hands.Hands()
-drawing_utils = mp.solutions.drawing_utils
-screen_width, screen_height = pyautogui.size()
-index_y = 0
+#libraries
+
+import cv2  #to open camera
+import mediapipe as mp  #AI for hand tracking
+import pyautogui  #GUI
+
+cap = cv2.VideoCapture(0) #open camera
+hand_detector = mp.solutions.hands.Hands() #detects hand
+drawing_utils = mp.solutions.drawing_utils  #detects finger joints
+screen_width, screen_height = pyautogui.size() #Screen size of application
+index_y = 0 #y-coordinate
 while True:
     _, frame = cap.read()
     frame = cv2.flip(frame, 1)
     frame_height, frame_width, _ = frame.shape
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) #color the joints
     output = hand_detector.process(rgb_frame)
-    hands = output.multi_hand_landmarks
+    hands = output.multi_hand_landmarks #landmarks (joints)
     if hands:
         for hand in hands:
             drawing_utils.draw_landmarks(frame, hand)
@@ -20,13 +23,13 @@ while True:
             for id, landmark in enumerate(landmarks):
                 x = int(landmark.x*frame_width)
                 y = int(landmark.y*frame_height)
-                if id == 8:
-                    cv2.circle(img=frame, center=(x,y), radius=10, color=(0, 255, 255))
+                if id == 8: #finger
+                    cv2.circle(img=frame, center=(x,y), radius=20, color=(0, 255, 255))
                     index_x = screen_width/frame_width*x
                     index_y = screen_height/frame_height*y
 
-                if id == 4:
-                    cv2.circle(img=frame, center=(x,y), radius=10, color=(0, 255, 255))
+                if id == 4: #thumb
+                    cv2.circle(img=frame, center=(x,y), radius=20, color=(0, 255, 255))
                     thumb_x = screen_width/frame_width*x
                     thumb_y = screen_height/frame_height*y
                     print('outside', abs(index_y - thumb_y))
